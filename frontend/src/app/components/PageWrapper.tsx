c8e2ce5580e47940e60b2afb9411030cb6da99bd
chuksremi15 <chuksremi15@gmail.com>
2025-03-13 13:03:37 +0100

    fix:adjusted view

--- frontend/src/app/components/PageWrapper.tsx
+++ frontend/src/app/components/PageWrapper.tsx
@@ -3,10 +3,10 @@ import { Navbar } from "../header/Navbar";
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className=" relative h-screen py-10 ">
+    <div className=" relative h-screen  ">
       <div
         style={{ scrollbarWidth: "none" }}
-        className="container h-[90vh] overflow-scroll py-4 px-4 mx-auto max-w-[432px] bg-white border border-gray rounded-2xl"
+        className="container h-full overflow-scroll py-4 px-4 mx-auto max-w-[432px] bg-white border border-gray rounded-2xl"
       >
         <Navbar />
         {children}
